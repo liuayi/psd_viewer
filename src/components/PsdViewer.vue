a2c545562923844d4c3221add8d8d5662ce14876
liuayi <2458372730@qq.com>
2024-09-27 16:15:48 +0800

    初始化psdview页面

---
+++ src/components/PsdViewer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+let title = ref('PSD文件预览')
+</script>
+
+<template>
+  <div class="PsdViewer">
+    <h1>
+      {{ title }}
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
