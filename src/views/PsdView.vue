b765d4d2baf789b222158d28b7ff609046d1e273
liuayi <2458372730@qq.com>
2024-09-28 00:02:48 +0800

    改掉ts类型报错，补充文件生成结果的数组维护

--- src/views/PsdView.vue
+++ src/views/PsdView.vue
@@ -1,63 +1,56 @@
 <script setup lang="ts">
 
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 let title = ref('PSD文件预览');
 import { InboxOutlined } from '@ant-design/icons-vue';
-import type { UploadChangeParam, UploadProps } from 'ant-design-vue';
+import type { UploadProps } from 'ant-design-vue';
 
-const fileList = ref<UploadProps['fileList']>([]);
+import { readPsd } from 'ag-psd';
+const fileList = ref<any[]>([]);
+const canvasList = ref<Array<{ uid: string, psd: ReturnType<typeof readPsd> }>>([]);
 
 const beforeUpload: UploadProps['beforeUpload'] = file => {
-  fileList.value.push(file)
+  if (fileList.value == undefined) {
+    fileList.value = []
+  }
+  fileList.value.push(file);
   console.log(fileList.value);
-  fileList.value.forEach((file) => {
-    // 读取文件内容为 ArrayBuffer
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    // 监听 FileReader 的 load 事件
-    reader.onload = (event) => {
-      // 获取 ArrayBuffer 数据
-      const arrayBuffer = reader.result;
-      const psd = readPsd(arrayBuffer);
-      document.body.appendChild(psd.children[0].canvas);
-      console.log('psd===', psd);
-      // 现在你可以使用这个 ArrayBuffer
-      // 例如，转换为十六进制表示或其他操作
-    };
-    // 错误处理
-    reader.onerror = (error) => {
-      console.error('读取文件时出错:', error);
-    };
-  })
-  return false;
-};
-
-// 自定义上传逻辑（不上传到服务器）
-const customRequest = ({ file, onSuccess }) => {
+  // 读取文件内容为 ArrayBuffer
   const reader = new FileReader();
-  reader.onload = (e) => {
-    const result = e.target?.result;
-    if (result) {
-      // 这里可以对文件进行后续处理
-      console.log('File content:', result);
-
-      // 调用 onSuccess 表示上传成功
-      onSuccess('success');
+  // new Blob()
+  reader.readAsArrayBuffer(file);
+  // 监听 FileReader 的 load 事件
+  reader.onload = () => {
+    // 获取 ArrayBuffer 数据
+    const arrayBuffer = reader.result;
+    if (arrayBuffer) {
+      const psd = readPsd(arrayBuffer as ArrayBuffer);
+      // document.body.appendChild(psd.children[0].canvas);
+      console.log('psd===', psd);
+      canvasList.value.push({ uid: file.uid, psd: psd });
     }
   };
+  // 错误处理
   reader.onerror = (error) => {
-    console.error('Error reading file:', error);
+    console.error('读取文件时出错:', error);
   };
-  reader.readAsDataURL(file);
+  return false;
 };
-const handleChange = (info: UploadChangeParam) => {
-  // console.log('handleChange', info);
-};
+const removeFile: UploadProps['onRemove'] = (file) => {
+  console.log('remove', file);
+  const uid = file.uid;
+  // 找到要删除对象的索引
+  const index = canvasList.value.findIndex(obj => obj.uid === uid);
+  if (index !== -1) {
+    // 使用 splice 方法删除对象
+    canvasList.value.splice(index, 1);
+  }
+}
+
 function handleDrop(e: DragEvent) {
   console.log(e);
 }
 
-import { readPsd } from 'ag-psd';
 </script>
 
 <template>
@@ -66,7 +59,7 @@ import { readPsd } from 'ag-psd';
       {{ title }}
     </h1>
     <a-upload-dragger v-model:fileList="fileList" name="file" accept=".psd" :multiple="true"
-      :beforeUpload="beforeUpload" @change="handleChange" @drop="handleDrop">
+      :beforeUpload="beforeUpload" @drop="handleDrop" @remove="removeFile">
       <p class="ant-upload-drag-icon">
         <inbox-outlined></inbox-outlined>
       </p>
@@ -75,6 +68,9 @@ import { readPsd } from 'ag-psd';
         只支持xxx.psd文件
       </p>
     </a-upload-dragger>
+    <a-carousel effect="fade">
+
+    </a-carousel>
   </div>
 </template>
 
