041d6ba20368871622a0db12ffc48341386515b0
liuayi <2458372730@qq.com>
2024-09-27 18:33:55 +0800

    psd整个流程串通，显示第一张图片

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Upload } from 'ant-design-vue'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(Upload)
 app.mount('#app')
